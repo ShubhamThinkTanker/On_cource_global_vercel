eaefd00d9626c64d2700af87178ffed1f4fe9b4a
shubham Vaishnav <shubham.thinktanker@gmail.com>
2022-10-19 15:20:17 +0530

    .vercel new 22

--- frontend/src/views/pages/admin/subjects/list/Index.jsx
+++ frontend/src/views/pages/admin/subjects/list/Index.jsx
@@ -63,7 +63,7 @@ const SubjectList = () => {
 					className="mr-1"
 					width="32"
 					height="32"
-					img={`${process.env.REACT_APP_SUBJECT_URL}/${row.subject_image}`}
+					img={`${process.env.REACT_APP_SUBJECT_URL}${row.subject_image}`}
 				/>
 			);
 		} else {
